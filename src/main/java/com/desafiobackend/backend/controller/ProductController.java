8f17bb05116bf610b5d580260d2e57f1dc68881b
Cauê Prado <caue_prado@hotmail.com>
2020-02-10 19:36:39 -0500

    swagger documentation

--- src/main/java/com/desafiobackend/backend/controller/ProductController.java
+++ src/main/java/com/desafiobackend/backend/controller/ProductController.java
@@ -3,6 +3,8 @@ package com.desafiobackend.backend.controller;
 import com.desafiobackend.backend.model.Product;
 import com.desafiobackend.backend.model.User;
 import com.desafiobackend.backend.service.ProductApplicationService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,6 +12,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+@Api
 @RestController
 @RequestMapping("products")
 @RequiredArgsConstructor
@@ -18,16 +21,19 @@ public class ProductController {
     private final ProductApplicationService productApplicationService;
 
     @PostMapping("{product}/{userId}")
+    @ApiOperation(value = "Relate a product to a user", tags = "User")
     private void relateProduct(@PathVariable("product") final String productName, @PathVariable("userId") final String userId) {
         productApplicationService.relateProduct(productName, userId);
     }
 
     @GetMapping("{product}")
+    @ApiOperation(value = "Find all users wich have a inscription to a product", tags = "User")
     private List<User> findUsersByProduct(@PathVariable("product") final String product) {
         return Collections.unmodifiableList(new ArrayList<>());
     }
 
     @GetMapping("{userId}")
+    @ApiOperation(value = "Find all products of a user", tags = "User")
     private List<Product> listProductsByUser(@PathVariable("userId") final String userId) {
         return Collections.unmodifiableList(new ArrayList<>());
     }
--- src/main/java/com/desafiobackend/backend/controller/UserController.java
+++ src/main/java/com/desafiobackend/backend/controller/UserController.java
@@ -1,15 +1,21 @@
 package com.desafiobackend.backend.controller;
 
+import com.desafiobackend.backend.exception.ExistingCPFException;
 import com.desafiobackend.backend.exception.UserNotFoundException;
 import com.desafiobackend.backend.mapper.UserRequestMapper;
 import com.desafiobackend.backend.model.User;
 import com.desafiobackend.backend.request.UserRequest;
 import com.desafiobackend.backend.service.UserApplicationService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api
 @RestController
 @RequestMapping("users")
 @RequiredArgsConstructor
@@ -20,28 +26,37 @@ public class UserController {
     private final UserRequestMapper userRequestMapper;
 
     @GetMapping("{userId}")
+    @ApiOperation(value = "Find user by id")
     public User find(@PathVariable("userId") final String userId) {
         return userApplicationService.findUserById(userId)
                 .orElseThrow(UserNotFoundException::new);
     }
 
     @GetMapping
+    @ApiOperation(value = "Find all existing users")
     public List<User> findAll() {
         return userApplicationService.findAll();
     }
 
     @PutMapping("{userId}/update")
+    @ApiOperation(value = "Updates an existing user")
     public User update(@PathVariable("userId") final UserRequest userRequest) {
         final User user = userRequestMapper.map(userRequest);
         return userApplicationService.updateUser(user);
     }
 
     @DeleteMapping("{userId}")
+    @ApiOperation(value = "Delete an existing user")
     public void delete(@PathVariable("userId") final String userId) {
         userApplicationService.deleteUser(userId);
     }
 
     @PostMapping
+    @ApiOperation(value = "Insert a new user")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "User successfully inserted", response = User.class),
+            @ApiResponse(code = 409, message = "Cpf already exists", response = ExistingCPFException.class)
+    })
     public User insert(final UserRequest userRequest) {
         final User newUser = userRequestMapper.map(userRequest);
         return userApplicationService.insertUser(newUser);
